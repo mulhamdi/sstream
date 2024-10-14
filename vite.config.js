a509c8cacd066c255abf03dd101653803c4d0d66
Mimu <124642469+mulhamdi@users.noreply.github.com>
2024-10-14 22:54:00 +0700

    feat: hopefully this will work ~tehe

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sstream/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
